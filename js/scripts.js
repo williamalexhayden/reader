de2344d882925267850c01fd63d02b3e5376393b
williamalexhayden <skutsch40@engr-134-19.eastern.ewu.edu>
2015-02-02 14:20:49 -0800

    commit 1

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// JavaScript to get hour in the day
+// REF: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+// get the hour of day
+var date = new Date();
+var current_hour = date.getHours();
